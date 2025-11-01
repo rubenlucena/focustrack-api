8f11c813b479c1ca768c84628ffe9b354ac278a9
rubenlucena <ruben.lucena@gmail.com>
2025-11-01 17:05:59 +0100

    Test changes

--- obj/Debug/net8.0/FocusTrack.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/FocusTrack.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FocusTrack.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+593fc227ceffe24ce698ae04d615100f45e1afc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FocusTrack.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FocusTrack.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
